36f9d0eb02a8012b36d7c52b527e1fb9d932a125
Aparecido Nicolini <aparecido.nicolini@hotmail.com>
2025-12-01 13:05:59 -0300

    update api

--- api/js.js
+++ api/js.js
@@ -1,7 +1,7 @@
 // https://jsonplaceholder.typicode.com/posts
 
 function clicou() {
-    fetch('https://jsonplaceholder.typicode.com/comments')
+    fetch('https://jsonplaceholder.typicode.com/posts')
         .then(response => response.json())
         .then(json => console.log(json))
 }
